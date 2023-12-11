5b8c511b84788a2b661c9f11c8f0f74acd1d8953
matrix-bro <razmhz11@gmail.com>
2023-12-11 13:04:23 +0545

    Contact creation done, but need to add send email functionality

--- app/api/contact.py
+++ app/api/contact.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from app.models.contact import Contact
 from rest_framework.views import APIView
 from rest_framework import permissions
+from rest_framework.response import Response
 
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,4 +13,18 @@ class ContactCreateView(APIView):
     permission_classes = (permissions.AllowAny, )
 
     def post(self, request, format=None):
-        pass    
+        data = self.request.data
+
+        try:
+            # send mail here
+
+            contact = Contact(first_name=data['first_name'], last_name=data['last_name'], email=data['email'], subject=data['subject'], message=data['message'])
+            contact.save()
+
+            return Response({
+                'success': 'Message sent successfully'
+            })
+        except:
+            return Response({
+                'error': 'Message failed to send'
+            })
