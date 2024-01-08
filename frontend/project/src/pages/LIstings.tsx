3483e9a49463049ae419dfd226be46654d67e76b
matrix-bro <razmhz11@gmail.com>
2024-01-08 12:13:48 +0545

    Displayed listings in Listings page

--- frontend/project/src/pages/LIstings.tsx
+++ frontend/project/src/pages/LIstings.tsx
@@ -1,5 +1,50 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ListingsInterface } from "../types";
+import Card from "../components/Card";
+
 const Listings = () => {
-  return <div>Listings</div>;
+  const [listings, setListings] = useState<ListingsInterface[]>([]);
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:8000/api/listings/?page=1"
+        );
+
+        setListings([...res.data.results]);
+      } catch (error) {}
+    };
+
+    fetchListings();
+  }, []);
+
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-8 px-8">
+        {listings.map((listing: ListingsInterface, index) => {
+          return (
+            <Card
+              key={index}
+              title={listing.title}
+              address={listing.address}
+              city={listing.city}
+              state={listing.state}
+              price={listing.price}
+              sale_type={listing.sale_type}
+              home_type={listing.home_type}
+              bedrooms={listing.bedrooms}
+              bathrooms={listing.bathrooms}
+              sqft={listing.sqft}
+              photo_main={listing.photo_main}
+              slug={listing.slug}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Listings;
---
+++ frontend/project/src/types.ts
@@ -0,0 +1,14 @@
+export interface ListingsInterface {
+  address: string;
+  bathrooms: number;
+  bedrooms: number;
+  city: string;
+  home_type: string;
+  photo_main: string;
+  price: number;
+  sale_type: string;
+  slug: string;
+  sqft: string;
+  state: string;
+  title: string;
+}
